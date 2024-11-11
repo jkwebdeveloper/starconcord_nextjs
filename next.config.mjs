ada9ca7f896ba2e04b1f1af6ac7d7b18c73f2fb3
Jignesh Koshti <118333099+jkwebdeveloper@users.noreply.github.com>
2024-11-11 18:46:26 +0530

    nextconfig changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     // output: 'export',
     reactStrictMode: false,
     images: {
-        domains: ["starconcord.com.in", "starconcord.onrender.com"],
-        // unoptimized: true,
+        domains: ["starconcord.com.in"],
+        unoptimized: true,
     },
 };
 
